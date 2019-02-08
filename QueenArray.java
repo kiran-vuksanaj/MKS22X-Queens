4b54387567c163883d0ab7abe73499279101e83a
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-02-08 09:48:54 -0500

    switched structure to arraylist, modified constructor

--- QueenArray.java
+++ QueenArray.java
@@ -1,11 +1,14 @@
+import java.util.ArrayList;
 public class QueenArray{
   public static void main(String[] args){
     //space for testing
   }
-  private int[] colVals;
+  private ArrayList<Integer> colVals;
+  private int size;
 
   public QueenArray(int size){
-    colVals = new int[size];
+    colVals = new ArrayList<Integer>();
+    this.size = size;
   }
   public boolean solve(){
     return false;
