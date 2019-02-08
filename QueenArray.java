8066b0ad47ddbc107970ab18b4f1e07248a19fbb
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-02-08 09:44:52 -0500

    base of file, alternate method of solving

---
+++ QueenArray.java
@@ -0,0 +1,16 @@
+public class QueenArray{
+  public static void main(String[] args){
+    //space for testing
+  }
+  private int[] colVals;
+
+  public QueenArray(int size){
+    colVals = new int[size];
+  }
+  public boolean solve(){
+    return false;
+  }
+  public int countSolutions(){
+    return -1;
+  }
+}
